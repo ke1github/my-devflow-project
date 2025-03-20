751cba8d5a0a22bb0bda9b072b3e394521e17793
Kosi Engineers <kosiengineers@gmail.com>
2025-03-20 08:25:14 +0530

    shancn addition

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl h1-bold font-inter bg-light-400 text-amber-100 ">
-        Welcome to the nexjs app 
-      </h1>
-    </div>
+    <>
+      <h1 className="text-3xl h1-bold font-inter ">Welcome to the nexjs app</h1>
+    </>
   );
 }

b51eae362b6841e5702bcfca4da7a3e415f94b0c
Kosi Engineers <kosiengineers@gmail.com>
2025-04-27 20:52:33 +0530

    Increment Question Details Views 4. using after()

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -8,14 +8,16 @@ import { formatNumber, getTimeStamp } from '@/lib/utils';
 import { RouteParams, Tag } from '@/types/global';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { after } from 'next/server';
 import React from 'react';
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
-  const [_, { success, data: question }] = await Promise.all([
-    await incrementViews({ questionId: id }),
-    await getQuestion({ questionId: id }),
-  ]);
+  const { success, data: question } = await getQuestion({ questionId: id });
+  // Increment views after fetching the question
+  after(async () => {
+    await incrementViews({ questionId: id });
+  });
 
   if (!success || !question) return redirect('/404');
 

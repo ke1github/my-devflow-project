f5c6988a72cc666e95f5e31d9810d21a5bcdcd14
Kosi Engineers <kosiengineers@gmail.com>
2025-05-04 20:54:41 +0530

    Implement Enhance Users' Answers with AI Feature 1.0

--- components/forms/AnswerForm.tsx
+++ components/forms/AnswerForm.tsx
@@ -78,10 +78,13 @@ const AnswerForm = ({ questionId, questionTitle, questionContent }: Props) => {
 
     setIsAISubmitting(true);
 
+    const userAnswer = editorRef.current?.getMarkdown();
+
     try {
       const { success, data, error } = await api.ai.getAnswer(
         questionTitle,
         questionContent,
+        userAnswer,
       );
 
       if (!success) {

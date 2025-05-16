b99323c6e5930150d4bbe4a2c2a7a7e68b0fa802
Kosi Engineers <kosiengineers@gmail.com>
2025-05-17 00:08:48 +0530

    Implement Edit Profile Feature 1.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import pluginReact from "eslint-plugin-react";
-import tailwindcss from "eslint-plugin-tailwindcss";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import pluginReact from 'eslint-plugin-react';
+import tailwindcss from 'eslint-plugin-tailwindcss';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -16,30 +16,30 @@ export default defineConfig([
     },
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: {
       js,
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
       react: pluginReact,
       tailwindcss,
     },
     extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
-      "plugin:tailwindcss/recommended",
-      "next/core-web-vitals",
-      "next/typescript",
-      "prettier",
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:tailwindcss/recommended',
+      'next/core-web-vitals',
+      'next/typescript',
+      'prettier',
     ],
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
-      "no-undef": "off",
+      'no-undef': 'off',
     },
   },
 ]);
